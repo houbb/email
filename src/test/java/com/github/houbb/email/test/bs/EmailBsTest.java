8696c58fe22c3ae7bb124262a1dec75ed5f20cc2
binbin.hou <1060732496@qq.com>
2020-01-06 09:29:08 +0800

    release branch 0.0.1

--- src/test/java/com/github/houbb/email/test/bs/EmailBsTest.java
+++ src/test/java/com/github/houbb/email/test/bs/EmailBsTest.java
@@ -1,6 +1,7 @@
 package com.github.houbb.email.test.bs;
 
 import com.github.houbb.email.bs.EmailBs;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -9,8 +10,14 @@ import org.junit.Test;
  */
 public class EmailBsTest {
 
+    /**
+     * 这里的用户名密码需要调整为用户自己的。
+     * 记得开通 SMTP 服务
+     * @since 0.0.1
+     */
     @Test
-    public void sendToTest() {
+    @Ignore
+    public void wangYi163Test() {
         EmailBs.auth("xxx@163.com", "xxx")
                 .content("Email for 2020 with bs", "新年好，我的老伙计")
                 .sendTo("xxx@yy.com");
